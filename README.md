9646a5c5afd07ef0a8cb1875e04bf665dac11a25
Jared Petersen <jpetersen11@wou.edu>
2015-10-21 10:52:47 -0700

    Changed css styling

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-IPEDSWebScraper
-===============
-IPEDWebScraper scrapes all of the terms and definitions in the IPEDS Glossary at http://nces.ed.gov/ipeds/glossary/ and outputs it as an HTML file that can be copied and pasted into a Wordpress Page (http://www.wou.edu/institutionalresearch/ipeds-glossary/)
+# IPEDSWebScraper
+IPEDWebScraper scrapes all of the terms and definitions in the IPEDS Glossary from http://nces.ed.gov/ipeds/glossary/ and outputs it as an HTML file that can be copied and pasted into a Wordpress page (http://www.wou.edu/institutionalresearch/ipeds-glossary/)
 
 Installation Requirements
 =========================
 Please have the following installed:
 - Python (program written in 2.7.6)
-- Beautiful Soup (http://www.crummy.com/software/BeautifulSoup/)
+- Beautiful Soup 4 (http://www.crummy.com/software/BeautifulSoup/)
 - Lxml (http://lxml.de/)
--- ipedscrape.py
+++ ipedscrape.py
@@ -40,37 +40,29 @@ def get_data(page, page_header):
           successful_pages.append(page_header)
 
           # Create the HTML table
-          write_list.append('<h1 id="' + page_header + '" style="margin-bottom: 0px;">' + page_header + '</h1>\n<table class="table_border1px" width="100%" cellspacing="0" cellpadding="5">\n<tbody>\n<tr bgcolor="maroon">\n<th class="table_head_2_reverse" style="color: white" align="left">Term</th>\n<th class="table_head_2_reverse" style="color: white" align="left">Definition</th>\n</tr>\n')
-
-          # Used to color the table rows
-          counter = 0
+          write_list.append('<h1 id="' + page_header + '" style="margin-bottom: 0px;">' + page_header + '</h1>\n<table style="border: 1px solid black;" width="100%" cellspacing="0" cellpadding="5">\n<tbody>\n<tr>\n<th style="border: 1px solid black;">Term</th>\n<th style="border: 1px solid black;">Definition</th>\n</tr>\n')
 
           # [1:] used to skip the header row
           for tr in soup.find_all('tr')[1:]:
                tds = tr.find_all('td')
-               write_data(tds[0].text.encode('utf-8'), tds[1].text.encode('utf-8'), counter)
-               counter += 1
+               # The encoding is technically ISO-8859-1, but that results in some mystery characters
+               write_data(tds[0].text.encode('utf-8'), tds[1].text.encode('utf-8'))
 
           # Close the table
           write_list.append('</tbody>\n</table>\n<br />\n')
 
-def write_data(term, definition, counter):
+def write_data(term, definition):
      """Write the input terms to the write_list with the proper row coloration
 
         Keyword arguments:
         term -- the IPED term
         defintion -- the definition of the IPED term
-        counter -- the current row number of the table, used to alternate row colors
      """
-     if counter % 2:
-          # Use colored rows
-          write_list.append('<tr bgcolor="#efefef">\n<td>' + term + '</td>\n<td>' + definition + '</td>\n</tr>\n')
-     else:
-          # Use white (default) rows
-          write_list.append('<tr>\n<td>' + term + '</td>\n<td>' + definition + '</td>\n</tr>\n')
+     write_list.append('<tr>\n<td style="border: 1px solid black;">' + term + '</td>\n<td style="border: 1px solid black;">' + definition + '</td>\n</tr>\n')
 
 def print_title(title_text):
-     """Print inputted text between a series of dashes to mark important text
+     """Print inputted text between a series of dashes to mark important text.
+        Used for outputting information to the user while the program is scraping data
 
         Keyword argument:
         title_text -- the text to be printed in a title format
@@ -91,7 +83,7 @@ if __name__ == '__main__':
 
      # Loop over all of the IPED pages
      for page in url_collection:
-          
+
           # Create page header but change the HTML header for the 0-9 page
           page_header = ' '
           if page == '0':
@@ -101,25 +93,26 @@ if __name__ == '__main__':
 
           # Get the IPED data and add it to the file
           get_data(page, page_header)
-          
+
           # Let the user know what's going on
           print "Processing IPED Glossary Pages - Page " + page_header + " Complete"
 
      # Add a top table navigation bar heading tag
-     # Not appending this to the write_list so that it can go on top   
+     # Not appending this to the write_list so that it can go on top
      html_file.write('<h2>')
-     
+
      # Loop over the successful pages and create a link to each
      for page in successful_pages:
           html_file.write('<a href="#' + page + '">' + page + '</a> ')
 
      # Finish writing the navigation bar to the file
      html_file.write('</h2>\n<br />\n')
+
      # Append the link to top to the write_list
      write_list.append('<br /><a href="#top">TOP</a><br /><br />')
      # Write the data tables to the file
      html_file.writelines(write_list)
 
-     # Close the file     
+     # Close the file
      html_file.close()
      print_title("IPED Glossary Web Scrape Complete")
